b48b627d6ac6055258f43a1fc2dd65f2665f75b7
Nick Whetstone <=>
2021-01-22 08:46:08 -0600

    Fix scripts/manage_translations.py typo 'runable'

--- scripts/manage_translations.py
+++ scripts/manage_translations.py
@@ -174,10 +174,10 @@ def fetch(resources=None, languages=None):
 
 
 if __name__ == "__main__":
-    RUNABLE_SCRIPTS = ('update_catalogs', 'lang_stats', 'fetch')
+    RUNNABLE_SCRIPTS = ('update_catalogs', 'lang_stats', 'fetch')
 
     parser = ArgumentParser()
-    parser.add_argument('cmd', nargs=1, choices=RUNABLE_SCRIPTS)
+    parser.add_argument('cmd', nargs=1, choices=RUNNABLE_SCRIPTS)
     parser.add_argument("-r", "--resources", action='append', help="limit operation to the specified resources")
     parser.add_argument("-l", "--languages", action='append', help="limit operation to the specified languages")
     options = parser.parse_args()
